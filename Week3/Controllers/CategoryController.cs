65d6979fd4a139b944f05bd9d42c7848595680c4
ontir <ontir@JR>
2024-03-20 09:49:55 +0200

    Add project files.

---
+++ Week3/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
